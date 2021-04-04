d692f1a8935ac49c15fb158452cbc9ba2efc09f7
Fedenko Maksym <feden2906@gmail.com>
2021-04-04 12:48:57 +0300

    add regexp for email, password and phone

--- constants/index.js
+++ constants/index.js
@@ -2,6 +2,7 @@ module.exports = {
   constants: require('./constants'),
   emailActions: require('./emailActions.enum'),
   modelNames: require('./modelNames.enum'),
+  regexpEnum: require('./regexp.enum'),
   statusCodes: require('./statusCodes.enum'),
   tableNames: require('./tableNames.enum')
 };
---
+++ constants/regexp.enum.js
@@ -0,0 +1,5 @@
+module.exports = {
+  EMAIL_REGEXP: new RegExp(/^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/),
+  PHONE_REGEXP: new RegExp(/^\+380\d{9}$/),
+  PASSWORD_REGEXP: new RegExp(/(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{6,}/)
+};
